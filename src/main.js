4bbe0f84ed77548e8e1059c869044c884a4aa554
xuzhihao <774910945@qq.com>
2021-03-09 17:30:26 +0800

    add axios

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import './plugins/axios';
 import { Button } from 'ant-design-vue';
 import App from './App.vue';
 import router from './router';
---
+++ src/mock/index.js
@@ -0,0 +1,3 @@
+const { mock } = require('mockjs');
+
+mock('', '');
